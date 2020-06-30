ea836e534d3b9788c92538cabbc45d6bc6d056d4
ozzhaas <kellenhaas@outlook.com>
2020-06-29 22:05:48 -0400

    6.29 10:05pm

---
+++ Lecture Examples/power.s
@@ -0,0 +1,47 @@
+/**
+    int i;
+    int power = 2
+
+    while(power <= 100)
+    {
+       printf("%d\n",  power);
+       power = power * 2;
+    }
+
+**/
+
+	.file	"power.s"
+	.text
+	.align	2
+	.global	main
+	.type	main, %function
+main:
+	push	{r4-r5, lr}
+
+    mov r4, #2         @ i = 2
+    mov r5, #100       @ limit
+
+
+
+loop:
+    cmp r4, r5
+    bgt finish
+
+    ldr     r0, =fmt
+    mov r1, r4
+    bl	printf
+    
+    mov r4, r4, lsl #1
+  
+    b loop
+
+finish:
+
+
+	mov	r0, #0
+	pop	{r4-r5, pc}
+
+	.section	.rodata
+	.align	2
+
+fmt: .ascii	"%d\n"
