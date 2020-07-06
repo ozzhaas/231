bcf2d042dfda87ee1c0ad5a39f076c29ff91595c
ozzhaas <mkhaas@g.clemson.edu>
2020-07-06 16:57:38 -0400

    7.6 4:57pm

---
+++ assignment1/prog1.m
@@ -0,0 +1,13 @@
+comment('Kellen Haas')
+comment('CPSC 2310 Sec. 1')
+comment('Programming Assignment 1')
+comment('7/6/20')
+
+
+comment('This program will convert a binary number to its equivalent decimal value.')
+
+
+word(N, 1101)
+
+
+
---
+++ assignment1/symbols.m
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+
+
+  
+
+
+
+   define(sum,0)
+   define(i,1)
+   define(j,2)
+   define(zero,3)
+   define(one,4)
+   define(ten,5)
+
+define(start,6)
+
+            
+define(loop,14)
+
+      
+         
+         
+   
+define(done,32)
+
+      
+
+
+   
