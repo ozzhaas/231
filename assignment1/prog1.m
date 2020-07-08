c51138b9bd65e303702f0d3aea7058b95a423fa5
ozzhaas <mkhaas@g.clemson.edu>
2020-07-07 23:13:29 -0400

    7.7 11:13pm

--- assignment1/prog1.m
+++ assignment1/prog1.m
@@ -6,8 +6,45 @@ comment('7/6/20')
 
 comment('This program will convert a binary number to its equivalent decimal value.')
 
+	word(N, 1101)
+	word(decVal, 0)
+	word(pwrOf2, 1)
+	word(temp, 0)
+	word(two, 2)
+	word(sum, 1)
+	word(zero, 0)
 
-word(N, 1101)
+label(start)
+	load(zero)
+	store(sum)
+	
+
+label(loop)
+	load(N)
+	div(10)
+	bgt0(done)
+	store(temp)
+	load(N)
+	sub(temp)
+	store(temp)
+	load(pwrOf2)
+	mul(temp)
+	store(temp)
+	load(sum)
+	add(temp)
+	store(sum)
+
+	load(pwrOf2)
+	mul(two)
+	store(pwrOf2)
+	ba(loop)	
+
+
+label(done)
+	print(sum)
+	halt
+
+end(start)	
 
 
 
--- assignment1/symbols.m
+++ assignment1/symbols.m
@@ -6,29 +6,24 @@
 
 
 
-  
+
+	define(N,0)
+	define(decVal,1)
+	define(pwrOf2,2)
+	define(temp,3)
+	define(two,4)
+	define(sum,5)
+	define(zero,6)
+
+define(start,7)
+			
+
+define(loop,11)
+													
+				
+
+define(done,45)
+		
 
 
 
-   define(sum,0)
-   define(i,1)
-   define(j,2)
-   define(zero,3)
-   define(one,4)
-   define(ten,5)
-
-define(start,6)
-
-            
-define(loop,14)
-
-      
-         
-         
-   
-define(done,32)
-
-      
-
-
-   
