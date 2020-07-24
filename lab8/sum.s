bb0f68348c9218c23887c4c0666ba8eb92a50983
ozzhaas <kellenhaas@outlook.com>
2020-07-23 20:45:54 -0400

    7.23 8:45pm

--- lab8/sum.s
+++ lab8/sum.s
@@ -7,10 +7,50 @@
 /* main() sums 10 values from stdin and prints the sum */
 main:
     push {lr}    // save lr
-    
-    //Code goes here
 
-    pop {pc}         // put lr in pc    
+    //Code goes here
+    add sp, sp, #-40 //10 integers each 4 bytes
+    mov r4, sp
+    mov r5, #0
+
+        firstLoop:
+            cmp r5, #10
+            bge finishFirst
+
+            ldr r0, =rdfmt
+            mov r1, r4
+            bl scanf
+
+            mov r4, r4, #4
+            add r5, r5, #1
+            b firstLoop
+
+        finishFirst:
+            mov r4, sp
+            mov r5, #1
+            add r6, #0
+
+        secondLoop:
+            cmp, r5, #10
+            bge finishSecond
+            ldr r0, [r4]
+            add r6, r6, r0
+
+            add r4, r4, #4
+            add r5, r5, #1
+            b secondLoop
+
+        finishSecond:
+            ldr r0, =prtfmt
+            mov r1, r6
+            bl printf
+
+            add sp, sp, #40
+
+
+
+
+    pop {pc}         // put lr in pc
 
 .section    .rodata
     rdfmt:        .asciz "%d"
