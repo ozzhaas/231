8a3b84dc7dae5f707ca943110962ee10430ac99c
ozzhaas <kellenhaas@outlook.com>
2020-08-01 21:17:39 -0400

    8.1 9:17pm

--- lab11/8bit-fp-table.c
+++ lab11/8bit-fp-table.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-  
+#include <math.h>
+
 int main()
 {
   int sign, exp, fraction, significand;
@@ -10,8 +11,33 @@ int main()
   for (i = 0; i < 256; i++)
   {
     /* begin your code */
+    sign = (i >> 7) & 1;    //asr the constant 7 by 1
+    exp = (i >> 3) & 15;    //asr the constant 3 by 15
 
+    fraction = i & 7;       //and i by 7 to get the fraction value
 
+    int de_normal = 0;      //initialize denormal variable
+    if (exp == 0) {         //check if the exponent is denormal
+        de_normal = 1;      //if yes, set denormal and exponent to 1
+        exp = 1;
+    }
+
+    exp = exp - 8;          //convert the exponent by subtracting 8
+
+    if (de_normal == 0) {   //set the bits for the full significand
+        significand = 8 | fraction;
+    }
+    else {
+        significand = fraction;
+    }
+
+    exp = exp - 3;
+
+    //calculate the float number
+    f = significand * pow(2, exp);
+    if (sign == 1) {
+        f = f * 1;
+    }
 
     /* end your code */
 
