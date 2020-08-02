5ae1e0fab3f34ebdd7f74fbecec2b58e38d37095
ozzhaas <kellenhaas@outlook.com>
2020-08-01 21:55:20 -0400

    8.1 9:55pm

--- assignment4/encode.s
+++ assignment4/encode.s
@@ -12,7 +12,14 @@
      .type encode, %function
 
 /********************************************************************
-    You are to write a subroutine for dealing with encoded messages using the following private-key algorithm: Consider each letter of a message and of a shared key as an integer from 1 to 26 (i.e., a=1, b=2, ..., z=26). The resulting letter in the encoded message is obtained by adding the letter values together and then subtracting 26 if the sum is greater than 26. The key is repeated until the complete message is encoded. Spaces, digits, and punctuation are not encoded but they do count towards the repetition of the key.
+    You are to write a subroutine for dealing with encoded messages using the
+    following private-key algorithm: Consider each letter of a message and of
+    a shared key as an integer from 1 to 26 (i.e., a=1, b=2, ..., z=26).
+    The resulting letter in the encoded message is obtained by adding the
+    letter values together and then subtracting 26 if the sum is greater than
+    26. The key is repeated until the complete message is encoded. Spaces,
+     digits, and punctuation are not encoded but they do count towards the
+     repetition of the key.
 ********************************************************************/
 
 
@@ -46,34 +53,35 @@ encode:
       sub r11, r11, #96
 
       mov r7, #0
-      add r7,r10, r11
+      add r7, r10, r11
 
       cmp r7, #26
       bgt subtract
 
-store:
+   store:
 
+      add r7, r7, #96
       str r7, [r1, r4]
 
       add r4, r4, #4
       add r5, r5, #4
 
-subtract:
+   subtract:
 
       sub r7, r7, #26
       bal store
 
 
-skip:
-    add r4, r4, #4
-    add r5, r5, #4
+   skip:
+      add r4, r4, #4
+      add r5, r5, #4
 
-  bal loop
+      bal loop
 
-restartKey:
-  mov r5, #0
-  bal label1
+   restartKey:
+      mov r5, #0
+      bal label1
 
 done:
 
-  bx lr
+      bx lr
